90396be734a80081aa726d982fbe8d5b01c19072
Xingjian-Li-117 <lixingjian_jane@hotmail.com>
2024-10-15 13:07:02 -0700

    1 - auth controllers added

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,7 +1,9 @@
 import express from 'express';
-import { login, signup, logout } from '../controllers/auth.controller.js';
+import { getMe, login, signup, logout } from '../controllers/auth.controller.js';
 
 const router = express.Router();
+
+router.get("/me", protectRoute, getMe);
 
 router.post("/signup", signup);
 
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,19 @@
-import express from "express";
-import dotenv from "dotenv";
+import express from 'express';
+import dotenv from 'dotenv';
 import authRoutes from "./routes/auth.routes.js"; // .js necessary
 import connectMongoDB from "./db/connectMongoDB.js";
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
 console.log(process.env.MONGO_URI);
 const app = express();
 const PORT = process.env.PORT || 8000;
+
+app.use(express.json()); // to parse req.body, middleware between req and response 
+app.use(express.urlencoded({ extended: true })) // to parse form data(urlencoded)
+
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
